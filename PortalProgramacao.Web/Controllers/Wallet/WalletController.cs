f097316bd284eb1a2f4ea1ba57c7b448f7c7d43b
Dalton Pessoa <dps3cin@gmail.com>
2022-10-28 11:49:43 -0300

    [DPS-003] continuacao do desenv

---
+++ PortalProgramacao.Web/Controllers/Wallet/WalletController.cs
@@ -0,0 +1,31 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PortalProgramacao.Web.Models;
+
+namespace PortalProgramacao.Web.Controllers.Wallet;
+
+public class WalletController : BaseController
+{
+    private readonly ILogger<WalletController> _logger;
+
+    public WalletController(ILogger<WalletController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
