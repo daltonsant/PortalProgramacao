8db14ac1d74624e24c7a86507e315ef420720bf7
Dalton Pessoa <dps3cin@gmail.com>
2022-12-20 07:42:12 -0300

    change color

--- PortalProgramacao.Web/wwwroot/js/Dashboard/dashboard.js
+++ PortalProgramacao.Web/wwwroot/js/Dashboard/dashboard.js
@@ -40,7 +40,7 @@
         let configuredChart = {};
         configuredChart.chart = myChart;
         configuredChart.config = {
-            color: ['#848f98', '#0063be', '#151515', '#61a60e'],
+            color: ['#53672A', '#0063be', '#ff5a00', '#002e58'],
             tooltip: {
                 trigger: 'axis',
                 axisPointer: {
@@ -113,198 +113,6 @@
 
         return configuredChart;
     }
-
-    function fnGetPendenciesPerCategoryChart(){
-        let chartDom = document.getElementById('pendeciasPerCategorias');
-        let myChart = echarts.init(chartDom);
-
-        let configuredChart = {};
-        configuredChart.chart = myChart;
-        configuredChart.config = {
-            color: ['#848f98', '#0063be', '#151515', '#61a60e'],
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    // Use axis to trigger tooltip
-                    type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
-                }
-            },
-            legend: {},
-            grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
-                containLabel: true
-            },
-            xAxis: {
-                type: 'value',
-                boundaryGap: [0, 0.02],
-                axisLabel : { show: false }, splitLine: { show: false }
-            },
-            yAxis: {
-                type: 'category'
-            },
-            dataset: {
-                // Provide a set of data.
-                source: [
-                    // ['Category', 'Não iniciada', 'Em andamento', 'Em revisão', 'Concluída'],
-                    // ['Civil', 43.3, 85.8, 93.7, 20.0],
-                    // ['Eletromecânico', 83.1, 73.4, 55.1, 40.0],
-                    // ['Aterramento', 86.4, 65.2, 82.5, 200.0],
-                    // ['Projeto', 72.4, 53.9, 39.1, 100.0],
-                    // ['Painéis', 72.4, 53.9, 39.1, 100.0],
-                    // ['Equipamentos', 72.4, 53.9, 39.1, 100.0],
-                    // ['Interligações', 72.4, 53.9, 39.1, 100.0],
-                    // ['SPCS', 72.4, 53.9, 39.1, 100.0]
-                ]
-            },
-            series: [
-                {
-                    type: 'bar',
-                    stack: 'total',
-                    label: {
-                        show: true,
-                        color: "#fff"
-                    }
-                },
-                {
-                    type: 'bar',
-                    stack: 'total',
-                    label: {
-                        show: true,
-                        color: "#fff"
-                    }
-                },
-                {
-                    type: 'bar',
-                    stack: 'total',
-                    label: {
-                        show: true,
-                        color: "#fff"
-                    }
-                },
-                {
-                    type: 'bar',
-                    stack: 'total',
-                    label: {
-                        show: true,
-                        color: "#fff"
-                    }
-                }
-            ]
-        };
-
-        return configuredChart;
-    }
-
-    function fnGetProgressChart() {
-        let chartDom = document.getElementById('progressPerEtapas');
-        let myChart = echarts.init(chartDom);
-
-        let configuredChart = {};
-        configuredChart.chart = myChart;
-        configuredChart.config = {
-            color: ['#848f98', '#0063be', '#151515', '#61a60e'],
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    // Use axis to trigger tooltip
-                    type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
-                }
-            },
-            legend: {},
-            grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
-                containLabel: true
-            },
-            xAxis: {
-                type: 'value',
-                boundaryGap: [0, 0.02],
-                axisLabel : { show: false }, splitLine: { show: false }
-            },
-            yAxis: {
-                type: 'category'
-            },
-            dataset: {
-                source: [
-                    // ['Status', 'Não iniciada', 'Em andamento', 'Em revisão', 'Concluída'],
-                    // ['Informativo', 43.3, 85.8, 93.7,55],
-                    // ['Acompanhamento', 83.1, 73.4, 55.1,55],
-                    // ['Pendência não impeditiva', 86.4, 65.2, 82.5,55],
-                    // ['Pendência impeditiva', 72.4, 53.9, 39.1,55],
-                    // ['Não conformidade', 72.4, 53.9, 39.1,55]
-                ]
-            },
-            series: [
-                {type: 'bar', stack: 'total', label: { show: true, color: "#fff" } },
-                { type: 'bar', stack: 'total', label: { show: true, color: "#fff" } },
-                { type: 'bar', stack: 'total', label: { show: true, color: "#fff" } },
-                { type: 'bar', stack: 'total', label: { show: true, color: "#fff" }}
-            ]
-        };
-
-        return configuredChart;
-    }
-
-    function fnGetProgressPerProject(){
-        let chartDom = document.getElementById('progressPerProjects');
-        let myChart = echarts.init(chartDom);
-
-        let configuredChart = {};
-        configuredChart.chart = myChart;
-        configuredChart.config = {
-            color: ['#9d9d9d','#0063be', '#ff5a00', '#61a60e'],
-            tooltip: {
-                trigger: 'item'
-            },
-            legend: {
-                orient: 'vertical',
-                left: 'left'
-            },
-            dataset: {
-                source : [
-                    // ['Projetos concluídos',100],
-                    // ['Projetos em andamento',200],
-                    // ['Projetos não iniciados',120],
-                ]
-            },
-            series: [
-                {
-                    type: 'pie',
-                    top: '10%',
-                    bottom : '0%',
-                    radius: '80%',
-                    center: ['60%', '50%'],
-                    label: {
-                        position: 'inner',
-                        fontSize: 12,
-                        color: "#fff",
-                        formatter: function(value) {
-                            let percent = Number.parseFloat(value.percent);
-
-                            if(percent === 0.0) return "";
-
-                            return percent.toFixed(1) + " %";
-                        }
-                    },
-                    labelLine: {
-                        show: false
-                    },
-                    emphasis: {
-                        itemStyle: {
-                            shadowBlur: 10,
-                            shadowOffsetX: 0,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)'
-                        }
-                    }
-                },
-            ]
-        };
-
-        return configuredChart;
-    }
     
     function nplPerProcessChart(){
         let chartDom = document.getElementById('nplPerProc');
@@ -313,7 +121,7 @@
         let configuredChart = {};
         configuredChart.chart = myChart;
         configuredChart.config = {
-            color: ['#53672A', '#5c881A', '#61A60E', '#FF5A00'],
+            color: ['#53672A', '#0063be', '#ff5a00', '#002e58'],
             tooltip: {
                 trigger: 'axis',
                 axisPointer: {
@@ -424,7 +232,7 @@
         let configuredChart = {};
         configuredChart.chart = myChart;
         configuredChart.config = {
-            color: ['#53672A', '#5c881A', '#61A60E', '#FF5A00'],
+            color: ['#53672A', '#0063be', '#ff5a00', '#002e58'],
             tooltip: {
                 trigger: 'axis',
                 axisPointer: {
